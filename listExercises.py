145b15d5b40fe9aa375ba47beba567c7b728c49c
Jim He <jimhe7@gmail.com>
2018-03-03 17:54:00 +0000

    work

--- listExercises.py
+++ listExercises.py
@@ -30,7 +30,7 @@ for i in range (0,len(list)):
         print(list[i])
         
 
-"""
+
 #ex6
 list2 =[]
 for i in range (0,len(list)):
@@ -38,3 +38,30 @@ for i in range (0,len(list)):
         list2.append(list[i])
         
 print(list2)
+
+
+
+#ex7
+factor = 3
+list3 = []
+for i in range (0,len(list)):
+    list3.append(list[i]*3)
+
+print(list3)
+
+
+
+#ex8
+vector1 = [13,4,3,2,4]
+vector2 = [3,4,5,6,32]
+vector3 = []
+for i in range (0,len(vector1)):
+    vector3.append(vector1[i]+vector2[i])
+    
+print(vector3)
+
+"""
+#ex9
+matrix1 = [[1,3],[2,4]]
+matrix2 = [[5,2],[1,0]]
+
