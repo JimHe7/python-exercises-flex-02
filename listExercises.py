dcd6be008a4f49fdf096343d4587a719dae05d7d
Jim He <jimhe7@gmail.com>
2018-03-03 17:40:49 +0000

    Inital

---
+++ listExercises.py
@@ -0,0 +1,40 @@
+# ex1 
+list = [2,3,4,1,3,-1,0,-5]
+
+"""
+sum = 0
+for i in range (0,len(list)):
+    sum +=list[i]
+    
+print(sum)
+
+
+#ex 2 
+
+print(max(list))
+
+#ex3 
+
+print(min(list))
+
+#ex4
+for i in range (0,len(list)):
+    if list[i] % 2 == 0:
+        print(list[i])
+        
+
+
+#ex5
+for i in range (0,len(list)):
+    if list[i] > 0:
+        print(list[i])
+        
+
+"""
+#ex6
+list2 =[]
+for i in range (0,len(list)):
+    if list[i] > 0:
+        list2.append(list[i])
+        
+print(list2)
