4ef0fa18319267e0bc98b5c7b5183497651d7435
Jim He <jimhe7@gmail.com>
2018-03-03 02:17:03 +0000

    exercise

--- exercise.py
+++ exercise.py
@@ -1,1 +1,35 @@
-print("hello")
+#ex 1
+
+#name = input("What is your name?")
+#print("Hello,", name)
+
+#ex 2
+#name = input("What is your name?")
+#print("Hello,", name.upper())
+#print("your name has".upper(),len(name),"LETTERS IN IT! AWESOME!")
+
+#ex3
+#print("Please fill in the the blanks below:")
+#phrase = "'s favorite food is "
+#print("____(name)___",phrase,"____(food)___",sep="")
+
+#name = input("What is your name? ")
+#food = input("What is your favorite food? ")
+#print(name,phrase,food,sep="")
+
+#ex4 works if entered line by line in terminal
+#week = {0:"Sunday",1:"Monday",2:"Tuesday",3:"Wednesday",4:"Thursday",5:"Friday",6:"Sunday"}
+#day = int(input('Day (0-6)? '))
+#print(week[day])
+
+#ex5
+#day = int(input('Day (0-6)? '))
+#if(day==0 or day==6):
+#    print("Sleep in")
+#else:
+#    print("Go to work")
+    
+#ex6
+C = float(input("Temperature in C?"))
+F = C *1.8 + 32
+print(F,"F")
