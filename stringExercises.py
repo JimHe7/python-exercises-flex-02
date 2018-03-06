6e0510b773dd808fc924eb328b2ce986f5a06a01
Jim He <jimhe7@gmail.com>
2018-03-06 05:57:29 +0000

    initial

--- stringExercises.py
+++ stringExercises.py
@@ -17,7 +17,7 @@ for i in range(l-1,-1,-1):
     
 #e4
 
-"""
+
 
 convert = {"A":"4", "E":"3", "G":"6", "I":"1", "O":"0", "S":"5", "T":"7"}
 phrase = list(input("phrase?"))
@@ -25,6 +25,39 @@ l = len(phrase)
 for i in range(0,l):
     if phrase[i] in convert.keys():
         phrase[i]=convert[phrase[i]]
+
+phraseString = "".join(phrase)
+print(phraseString)
+
+"""
+
+#e6
+
+#use ascii code to cycle through alphabet 
+# "a" is 97, "z" is 122
+#at code 110 need to shift back to 97 since moving out of 26 letter range
+
+code = []
+
+for n in range(97,110):
+    code.append(chr(n+13))
+    
+for k in range(110,123):
+    code.append(chr(k-13))
+
+#generate list of alphabet, combine both lists into a dictionary
+
+alphabet = []
+for a in range(97,123):
+    alphabet.append(chr(a))
+
+translator = dict(zip(code,alphabet))
+
+phrase = list(input("phrase? "))
+l = len(phrase)
+
+for i in range(0,l):
+    phrase[i]=translator[phrase[i]]
 
 phraseString = "".join(phrase)
 print(phraseString)
